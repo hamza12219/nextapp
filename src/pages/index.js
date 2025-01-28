14f9ec8e401f14225802e4ebfdc2fe9c8b895906
hamza12219 <hamza019475@gmail.com>
2025-01-28 22:27:22 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -172,7 +172,7 @@ const Home = () => {
         </div>
 
         {/* Grid of Filtered Articles */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6" style={{margin:'auto', marginLeft:'30px'S}}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6" style={{margin:'auto', marginLeft:'30px'}}>
           {filteredArticles.map((article) => (
             <div key={article.id} className="bg-white rounded-lg shadow-lg overflow-hidden">
               {/* Image */}
