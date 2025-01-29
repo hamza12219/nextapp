d6a54c07fed2b00fabf10a85fafea9c52b5425ab
hamza12219 <hamza019475@gmail.com>
2025-01-29 20:56:36 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -209,7 +209,7 @@ const clearDateRange = () => {
         </div>
 
         {/* Grid of Filtered Articles */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6" style={{margin:'auto', width: '90%'}}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 align-center" style={{margin:'auto', width: '90%'}}>
           {filteredArticles.map((article) => (
             <div key={article.id} className="bg-white rounded-lg shadow-lg overflow-hidden">
               {/* Image */}
